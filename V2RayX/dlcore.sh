cc5a5090b30b1e896112012a7f5f10b3f51d4088
xuaihua <simpleman1984@126.com>
2017-12-08 11:25:51 +0800

    适配到v2ray 3.1

--- V2RayX/dlcore.sh
+++ V2RayX/dlcore.sh
@@ -1,4 +1,4 @@
-VERSION="v2.41"
+VERSION="v3.1"
 
 
 cd $SRCROOT
@@ -8,13 +8,16 @@ if [[ -f ./v2ray-core-bin/v2ray ]]; then
 fi
 existingVersion=${output:6:${#VERSION}}
 if [ "$VERSION" != "$existingVersion" ]; then
-    curl -s -L --create-dirs -o v2ray-core-bin/v2ray-macos.zip https://github.com/v2ray/v2ray-core/releases/download/${VERSION}/v2ray-macos.zip
+#curl -s -L --create-dirs -o v2ray-core-bin/v2ray-macos.zip https://github.com/v2ray/v2ray-core/releases/download/${VERSION}/v2ray-macos.zip
     if [[ $? == 0 ]]; then
         cd v2ray-core-bin
         unzip -o v2ray-macos.zip
-        mv v2ray-${VERSION}-macos/v2ray v2ray
+        cp v2ray-${VERSION}-macos/v2ray v2ray
+        cp v2ray-${VERSION}-macos/v2ctl v2ctl
+        cp v2ray-${VERSION}-macos/geosite.dat geosite.dat
         chmod +x ./v2ray
-        rm -r v2ray-*
+        chmod +x ./v2ctl
+# rm -r v2ray-*
         exit 0
     else
         echo "download failed!"
